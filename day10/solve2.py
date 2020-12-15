57fb624c0573423af9c16403640b46ba91525f62
tomaszmj <tomasz.m.j.nowak@gmail.com>
2020-12-15 13:51:16 +0100

    solve day10 part2 (fianlly done)

--- day10/solve2.py
+++ day10/solve2.py
@@ -1,3 +1,5 @@
+import functools
+import operator
 from typing import List, Iterable
 
 
@@ -10,61 +12,52 @@ def yield_subsets(numbers: List[int]) -> Iterable[List[int]]:
         yield retval
 
 
-def is_correct(numbers: Iterable[int]) -> bool:
-    last = 0
-    for n in numbers:
-        if n - last > 3 or n - last < 0:
+def is_correct(numbers: List[int]) -> bool:
+    if len(numbers) == 0:
+        return False
+    last_number = numbers[0]
+    for n in numbers[1:]:
+        if n - last_number > 3 or n - last_number < 0:
             return False
-        last = n
+        last_number = n
     return True
 
 
-def split_into_unremovable_and_removable(numbers: List[int]) -> (List[int], List[int]):
-    unremovable = []
-    removable = []
+# splits list into sublists divided by "unremovable" elements
+# "unremovable" elements are the ones that have difference of 3 from one of neighbours
+def split(numbers: List[int]) -> List[List[int]]:
+    retval = []
+    current_sublist = []
     for i, n in enumerate(numbers):
         prev = numbers[i - 1] if i > 0 else 0
         next = numbers[i + 1] if i < len(numbers) - 1 else n + 3
         if n - prev == 3 or next - n == 3:
-            unremovable.append(i)
+            if len(current_sublist) > 0:
+                retval.append(current_sublist)
+                current_sublist = []
         else:
-            removable.append(i)
-    return unremovable, removable
-
-
-def numbers_subset(numbers: List[int], removed_indices: List[int]) -> Iterable[int]:
-    r = 0
-    for i, n in enumerate(numbers):
-        if r >= len(removed_indices):
-            yield n
-        elif removed_indices[r] == i:
-            r += 1
-        else:
-            yield n
-
-
-def brute_force_count(numbers: List[int]) -> int:
-    #return functools.reduce(operator.add, (int(is_correct(s)) for s in yield_subsets(numbers)))
-    count = 0
-    for subset in yield_subsets(numbers):
-        if is_correct(subset):
-            count += 1
-    return count
+            current_sublist.append(i)
+    return retval
 
 
 def solve(numbers: List[int]) -> int:
     numbers.sort()
-    unremovable, removable = split_into_unremovable_and_removable(numbers)
-    count = 0
-    for removed_indices in yield_subsets(removable):
-        if is_correct(numbers_subset(numbers, removed_indices)):
-            count += 1
+    #print(numbers, end="\n\n")
+    count = 1
+    for slice in split(numbers):
+        prev = numbers[slice[0] - 1] if slice[0] > 0 else 0
+        next = numbers[slice[-1] + 1] if slice[-1] < len(numbers) - 1 else numbers[-1] + 3
+        fragment = list(numbers[s] for s in slice)
+        subsets = yield_subsets(fragment)
+        subsequence_count = functools.reduce(operator.add, (int(is_correct([prev] + s + [next])) for s in subsets))
+        #print(f"({prev}) {fragment} ({next}) -> {subsequence_count}")
+        count *= subsequence_count
     return count
     
 
 def main():
     numbers: List[int] = []
-    with open("data_small.txt") as file:
+    with open("data.txt") as file:
         numbers = list(int(line.strip()) for line in file) 
     print(solve(numbers))
 
