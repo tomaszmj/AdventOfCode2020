8265d64bf0fadc546acbd479b596f1d5cec886db
tomaszmj <tomasz.m.j.nowak@gmail.com>
2021-04-20 21:41:00 +0200

    Solve day17 part1

--- day17/solve1.py
+++ day17/solve1.py
@@ -1,3 +1,8 @@
+from __future__ import annotations
+
+import functools
+import itertools
+import operator
 from typing import NamedTuple, Set, Iterable
 
 
@@ -11,13 +16,16 @@ class Point(NamedTuple):
     z: int
 
 
-# Euclidean pinates:
+# Space represents ACTIVE/INACTIVE points in infinite discrete Euclidean space:
 #   y
 #   ^
 #   |
 #   --> x
 #  /
 # z
+# Initially all points are INACTIVE, so we store only the ACTIVE ones in a set,
+# together with upper/lower bounds, i.e. lowest/highest coordinates for which
+# point was seen active (thanks to it we can iterate for each Conway box).
 class Space:
     def __init__(self):
         self.lower = Point(0, 0, 0)
@@ -50,15 +58,32 @@ class Space:
 
     def set_inactive(self, p: Point):
         if p in self.actives:
-            # we do not modify lower/upper bounds here, as it is not necessary
-            # (bounds are only to simplify iteration, space is infinite)
+            # We do not modify lower/upper bounds here, as it is not necessary and would
+            # require time-consuming search of new bounds or proper data structure for such search.
+            # Bounds can be larger than they should, as they are only to simplify iteration (space is infinite).
             self.actives.remove(p)
 
     def yield_all_points(self) -> Iterable[Point]:
-        for z in range(self.lower.z, self.upper.z + 1, 1):
-            for y in range(self.lower.y, self.upper.y + 1, 1):
-                for x in range(self.lower.x, self.upper.x + 1, 1):
-                    yield Point(x, y, z)
+        for p in itertools.product(
+            range(self.lower.x, self.upper.x + 1, 1),
+            range(self.lower.y, self.upper.y + 1, 1),
+            range(self.lower.z, self.upper.z + 1, 1)
+        ):
+            yield Point(p[0], p[1], p[2])
+
+    def yield_all_points_with_outliers(self) -> Iterable[Point]:
+        for p in itertools.product(
+                range(self.lower.x - 1, self.upper.x + 2, 1),
+                range(self.lower.y - 1, self.upper.y + 2, 1),
+                range(self.lower.z - 1, self.upper.z + 2, 1)
+        ):
+            yield Point(p[0], p[1], p[2])
+
+    @staticmethod
+    def yield_point_neighbours(p: Point) -> Iterable[Point]:
+        for diff in itertools.product((-1, 0, 1), repeat=3):
+            if diff != (0, 0, 0):
+                yield Point(p.x + diff[0], p.y + diff[1], p.z + diff[2])
 
     def string(self, z: int) -> str:
         return "\n".join(
@@ -67,45 +92,39 @@ class Space:
         )
 
     def count_active_neighbours(self, p: Point) -> int:
-        count = 0
-        for dz in [-1, 0, 1]:
-            for dy in [-1, 0, 1]:
-                for dx in [-1, 0, 1]:
-                    if dx == dy == dz == 0:
-                        continue
-                    if self.get(Point(p.x + dx, p.y + dy, p.z + dz)) == ACTIVE:
-                        count += 1
-        return count
+        return functools.reduce(operator.add, (1 if self.get(n) == ACTIVE else 0 for n in self.yield_point_neighbours(p)))
 
-    def transform_to(self, dst):
-        pass
+    def transform_to(self, dst: Space):
+        for p in self.yield_all_points_with_outliers():
+            active_neighbours = self.count_active_neighbours(p)
+            if self.get(p) == ACTIVE:
+                if active_neighbours == 2 or active_neighbours == 3:
+                    dst.set_active(p)
+                else:
+                    dst.set_inactive(p)
+            else:
+                if active_neighbours == 3:
+                    dst.set_active(p)
+                else:
+                    dst.set_inactive(p)
+
+    def count_actives(self) -> int:
+        return len(self.actives)
 
 
 def main():
     spaces = [Space(), Space()]
-    with open("data_small.txt") as f:
+    with open("data.txt") as f:
         for line in f:
             line = line.strip()
             if len(line) > 0:
                 spaces[0].add_row(line)
-                spaces[1].add_row(line)
-    print(f"{spaces[0].string(0)}\n\n")
-
-    print("lower bound", spaces[0].lower.x, spaces[0].lower.y, spaces[0].lower.z)
-    print("upper bound", spaces[0].upper.x, spaces[0].upper.y, spaces[0].upper.z)
-
-    for p in spaces[0].yield_all_points():
-        print(f"active neighbours of {p}: {spaces[0].count_active_neighbours(p)}")
-    # copied from day11: TODO change
-    # i = 0
-    # while True:
-    #     src = spaces[i % 2]
-    #     dst = spaces[(i + 1) % 2]
-    #     array_changed = src.transform(dst)
-    #     i += 1
-    #     if not array_changed:
-    #         print(f"array stopped changing after {i} iterations, with {src.count_all_occupied()} seats occupied")
-    #         return
+    iterations = 6
+    for i in range(iterations):
+        src = spaces[i % 2]
+        dst = spaces[(i + 1) % 2]
+        src.transform_to(dst)
+    print(f"after {iterations} iterations: {spaces[iterations % 2].count_actives()}")
 
 
 if __name__ == "__main__":
