37de227ec36e5ee6ebe3973cb07398e5895e83a3
tomaszmj <tomasz.m.j.nowak@gmail.com>
2021-07-25 22:20:40 +0200

    Remove old Tile and start adapting TileNeighbourhoodManager to new Tile with matrix transfomrations

--- day20/solve2.py
+++ day20/solve2.py
@@ -14,7 +14,7 @@ class Matrix2x2:
     # all_transformations will be filled in by precompute_transformations.
     # This is list of all possible tile transformations in this puzzle:
     # combined rotations and flipping in 2 axes.
-    all_transformations: List[Matrix2x2] = []
+    _all_transformations: List[Matrix2x2] = []
 
     def __init__(self, a: int, b: int, c: int, d: int):
         self.a: int = a
@@ -23,7 +23,13 @@ class Matrix2x2:
         self.d: int = d
 
     @classmethod
-    def precompute_transformations(cls):
+    def all_transformations(cls) -> List[Matrix2x2]:
+        if len(cls._all_transformations) == 0:
+            cls._precompute_transformations()
+        return cls._all_transformations
+
+    @classmethod
+    def _precompute_transformations(cls):
         rotate = Matrix2x2(0, 1, -1, 0)
         flip_horizontal_axis = Matrix2x2(1, 0, 0, -1)
         flip_vertical_axis = Matrix2x2(-1, 0, 0, 1)
@@ -41,11 +47,10 @@ class Matrix2x2:
     
     @classmethod
     def add_to_all_transformations(cls, m: Matrix2x2):
-        for t in cls.all_transformations:
+        for t in cls._all_transformations:
             if t == m:
                 return
-        cls.all_transformations.append(m)
-
+        cls._all_transformations.append(m)
 
     # multiply_left returns m * self
     def multiply_left(self, m: Matrix2x2) -> Matrix2x2:
@@ -82,12 +87,12 @@ class Matrix2x2:
         return f"[{self.a} {self.b} | {self.c} {self.d}]"
 
     def __eq__(self, other):
-        if isinstance(other, Matrix2x2):
-            return self.a == other.a and \
-                self.b == other.b and \
-                self.c == other.c and \
-                self.d == other.d
-        return False
+        if not isinstance(other, Matrix2x2):
+            return False
+        return self.a == other.a and \
+            self.b == other.b and \
+            self.c == other.c and \
+            self.d == other.d
 
 
 
@@ -97,7 +102,7 @@ def safe_add_to_dict_of_sets(dictionary: Dict[object, Set[object]], key, value):
     dictionary[key].add(value)
 
 
-class Tile2:
+class Tile:
     # Tile data coordiante systsem is constructed in a way that allows
     # to transform tile by just changing transformation matrix.
     # Transformation matrices represent linear transformations in 2D space.
@@ -125,10 +130,10 @@ class Tile2:
             for c in line:
                 self.data[(x, y)] = c
                 x += 1
-                if x == 0:  # skip x = 0
+                if x == 0:  # skip x = 0 for symmetry described above
                     x += 1
             y += 1
-            if y == 0:
+            if y == 0:  # skip y = 0 for symmetry described above
                 y += 1
 
     # get reads given position from Tile data.
@@ -163,92 +168,9 @@ class Tile2:
         return "".join(self.get(x, y, transformation) for y in all_y)
 
 
-@dataclass
-class TileTransformation:
-    rotation: int = 0
-    flip_up_down: int = 0
-    flip_left_right: int = 0
-
-    def __hash__(self):
-        # hash as 4-bit integer: rotation (0-3): bits 0,1, flip_up_down: bit 2, flip_left_right: bit 3
-        return self.rotation + (self.flip_up_down << 2) + (self.flip_left_right << 3)
-
-    @classmethod
-    def from_hash(cls, hash: int) -> TileTransformation:
-        rotation = hash & 0b0011
-        flip_up_down = (hash & 0b0100) >> 2
-        flip_left_right = (hash & 0b1000) >> 3
-        return TileTransformation(rotation, flip_up_down, flip_left_right)
-
-
-class Tile:
-    def __init__(self, text: List[str], number: int):
-        self.number = number
-        self.up = list(text[0])  # up in direction left->right - first line
-        self.right = list((t[-1] for t in text))  # right in direction up->down - last character from each line
-        self.down = list(text[-1])  # down in direction left->right - last line
-        self.left = list((t[0] for t in text))  # left in direction up->down - first character from each line
-        self.transformation = TileTransformation()
-
-    # rotates tile clockwise once
-    def rotate(self):
-        self.up, self.right, self.down, self.left = self.left, self.up, self.right, self.down
-        self.up.reverse()
-        self.down.reverse()
-        self.transformation.rotation = (self.transformation.rotation + 1) % 4
-
-    def flip_up_down(self):
-        self.up, self.down = self.down, self.up
-        self.left.reverse()
-        self.right.reverse()
-        self.transformation.flip_up_down = (self.transformation.flip_up_down + 1) % 2
-
-    def flip_left_right(self):
-        self.left, self.right = self.right, self.left
-        self.up.reverse()
-        self.down.reverse()
-        self.transformation.flip_left_right = (self.transformation.flip_left_right + 1) % 2
-
-    def set_transformation(self, transformation: TileTransformation):
-        while self.transformation.rotation != transformation.rotation:
-            self.rotate()
-        if self.transformation.flip_up_down != transformation.flip_up_down:
-            self.flip_up_down()
-        if self.transformation.flip_left_right != transformation.flip_left_right:
-            self.flip_left_right()
-
-    # warning, yield_all_transformations modifies Tile in place, so something like
-    # list(tile.yield_all_transformations()) will not work
-    def yield_all_transformations(self) -> Iterable[Tile]:
-        for rotation in range(4):  # for each rotation
-            yield self  # without flipping
-            self.flip_left_right()
-            yield self  # with flip_left_right
-            self.flip_left_right()  # reverts flip_left_right
-            self.flip_up_down()
-            yield self  # with flip_up_down
-            self.flip_left_right()
-            yield self  # with flip_up_down and flip_left_right
-            self.flip_left_right()  # reverts flip_left_right
-            self.flip_up_down()  # reverts flip_up_down
-            self.rotate()  # go to next rotation - or (in case of last iteration) reset to original rotation
-
-
-@dataclass
-class TileSelection:
+class TileSelection(NamedTuple):
     number: int
-    transformation: TileTransformation
-
-    def __hash__(self):
-        # transformation hash is 0-15, so by moving number 4 bits to the left,
-        # we get a unique value for each (number, transformation) pair
-        return self.transformation.__hash__() + (self.number << 4)
-
-    def __str__(self):
-        return f"{self.number},{self.transformation.__hash__()}"
-
-    def __repr__(self):
-        return f"{self.number},{self.transformation.__hash__()}"
+    transformation_index: int
 
 
 class TileWithDegree(NamedTuple):
@@ -274,20 +196,20 @@ class TileNeighbourhoodManager:
         for t1, t2 in itertools.product(tiles, repeat=2):
             if t1.number == t2.number:
                 continue
-            for tt1 in t1.yield_all_transformations():
-                for tt2 in t2.yield_all_transformations():
-                    if tt1.right == tt2.left:  # tt2 in current state might be left to tt1
-                        tt1_state = TileSelection(tt1.number, copy.deepcopy(tt1.transformation))
-                        tt2_state = TileSelection(tt2.number, copy.deepcopy(tt2.transformation))
-                        safe_add_to_dict_of_sets(self.right_successors, tt1_state, tt2_state)
-                        safe_add_to_dict_of_sets(possible_neighbours, tt1.number, tt2.number)
-                        safe_add_to_dict_of_sets(possible_neighbours, tt2.number, tt1.number)
-                    if tt1.down == tt2.up:  # tt2 in current state might be under tt1
-                        tt1_state = TileSelection(tt1.number, copy.deepcopy(tt1.transformation))
-                        tt2_state = TileSelection(tt2.number, copy.deepcopy(tt2.transformation))
-                        safe_add_to_dict_of_sets(self.down_successors, tt1_state, tt2_state)
-                        safe_add_to_dict_of_sets(possible_neighbours, tt1.number, tt2.number)
-                        safe_add_to_dict_of_sets(possible_neighbours, tt2.number, tt1.number)
+            for tr1_index, tr1 in enumerate(Matrix2x2.all_transformations()):
+                for tr2_index, tr2 in enumerate(Matrix2x2.all_transformations()):
+                    if t1.right(tr1) == t2.left(tr2):  # t2 in current transformation might be left to t1
+                        selection1 = TileSelection(t1.number, tr1_index)
+                        selection2 = TileSelection(t2.number, tr2_index)
+                        safe_add_to_dict_of_sets(self.right_successors, selection1, selection2)
+                        safe_add_to_dict_of_sets(possible_neighbours, t1.number, t2.number)
+                        safe_add_to_dict_of_sets(possible_neighbours, t2.number, t1.number)
+                    if t1.down(tr1) == t2.up(tr2):  # t2 in current transformation might be under t1
+                        selection1 = TileSelection(t1.number, tr1_index)
+                        selection2 = TileSelection(t2.number, tr2_index)
+                        safe_add_to_dict_of_sets(self.down_successors, selection1, selection2)
+                        safe_add_to_dict_of_sets(possible_neighbours, t1.number, t2.number)
+                        safe_add_to_dict_of_sets(possible_neighbours, t2.number, t1.number)
         for n, s in possible_neighbours.items():
             self.tiles_with_degrees.append(TileWithDegree(number=n, degree=len(s)))
         self.tiles_with_degrees.sort(key=lambda t: t.degree)
@@ -297,25 +219,25 @@ class TileNeighbourhoodManager:
         for td in self.tiles_with_degrees:
             print(f"{td.number}: {td.degree}")
 
-    def possible_new_tiles(self, left_neighbour: Tile, up_neighbour: Tile, chosen_numbers_set: Set[int]) -> Iterable[TileSelection]:
-        if left_neighbour and up_neighbour:
-            left_candidates = self.right_successors_per_tile(left_neighbour)
-            up_candidates = self.down_successors_per_tile(up_neighbour)
-            for selection in left_candidates.intersection(up_candidates):
-                if selection.number not in chosen_numbers_set:
-                    yield selection
-        elif left_neighbour:
-            for selection in self.right_successors_per_tile(left_neighbour):
-                if selection.number not in chosen_numbers_set:
-                    yield selection
-        elif up_neighbour:
-            for selection in self.down_successors_per_tile(up_neighbour):
-                if selection.number not in chosen_numbers_set:
-                    yield selection
-        else:  # no neighbours - yield all possible transformations of all tiles
-            for tile_number, transformation_hash in itertools.product(
-                    filter(lambda n: n not in chosen_numbers_set, self.all_tile_numbers), range(16)):
-                yield TileSelection(tile_number, TileTransformation.from_hash(transformation_hash))
+    # def possible_new_tiles(self, left_neighbour: Tile, up_neighbour: Tile, chosen_numbers_set: Set[int]) -> Iterable[TileSelection]:
+    #     if left_neighbour and up_neighbour:
+    #         left_candidates = self.right_successors_per_tile(left_neighbour)
+    #         up_candidates = self.down_successors_per_tile(up_neighbour)
+    #         for selection in left_candidates.intersection(up_candidates):
+    #             if selection.number not in chosen_numbers_set:
+    #                 yield selection
+    #     elif left_neighbour:
+    #         for selection in self.right_successors_per_tile(left_neighbour):
+    #             if selection.number not in chosen_numbers_set:
+    #                 yield selection
+    #     elif up_neighbour:
+    #         for selection in self.down_successors_per_tile(up_neighbour):
+    #             if selection.number not in chosen_numbers_set:
+    #                 yield selection
+    #     else:  # no neighbours - yield all possible transformations of all tiles
+    #         for tile_number, transformation_hash in itertools.product(
+    #                 filter(lambda n: n not in chosen_numbers_set, self.all_tile_numbers), range(16)):
+    #             yield TileSelection(tile_number, TileTransformation.from_hash(transformation_hash))
 
     def right_successors_per_tile(self, tile: Tile) -> Set[TileSelection]:
         key = TileSelection(tile.number, tile.transformation)
@@ -407,7 +329,6 @@ class Image:
 
 def main():
     tiles: List[Tile] = []
-    tiles2: List[Tile2] = []
     with open("data.txt") as f:
         line = f.readline().strip()
         while line:
@@ -419,29 +340,12 @@ def main():
                 line = f.readline().strip()
                 data.append(line)
             tiles.append(Tile(data, number))
-            tiles2.append(Tile2(data, number))
             line = f.readline().strip()
             if line:
                 raise ValueError(f"expected empty line, got {line}")
             line = f.readline().strip()
-
-    result_old_up = set()
-    tile = tiles[0]
-    for tt in tile.yield_all_transformations():
-        result_old_up.add("".join(tt.up))
-    print(sorted(list(result_old_up)))
-
-    Matrix2x2.precompute_transformations()
-    tile2 = tiles2[0]
-    result_new_up = set()
-    for t in Matrix2x2.all_transformations:
-        result_new_up.add(tile2.up(t))
-    print(sorted(list(result_new_up)))
-
-    for tt in Matrix2x2.all_transformations:
-        print(tt)
-    # neighbourhood_manager = TileNeighbourhoodManager(tiles)
-    # neighbourhood_manager.stats()
+    neighbourhood_manager = TileNeighbourhoodManager(tiles)
+    neighbourhood_manager.stats()
     # image = Image(tiles)
     # chosen_tiles = image.solve()
     # if chosen_tiles:
