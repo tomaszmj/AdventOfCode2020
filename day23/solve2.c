90d1b7065e01eb69d069336842df26fbaf35bc3b
tomaszmj <tomasz.m.j.nowak@gmail.com>
2021-08-18 00:45:01 +0200

    Day23 part1 in C

--- day23/solve2.c
+++ day23/solve2.c
@@ -21,12 +21,15 @@ int parse_input(int argc, char **argv, InputData *in);
 // returns dynamically allocated array of len total_len+1 or NULL pointer on error
 uint32_t* init_followers(InputData *in);
 
-// changes followers state according to the game rules
-void play(uint32_t followers[], uint32_t current_item, int iterations);
+// functions for changing followers state according to the game rules
+void play(uint32_t followers[], int total_len, uint32_t current_item, int iterations);
+uint32_t select_destination(uint32_t current_item, int total_len, uint32_t picked[PICKED_LEN]);
 
+// functions for shownig output
+void print_followers(uint32_t followers[], int total_len);
+void print_all_elements(uint32_t followers[], int total_len, int start, char *sep);
 uint32_t puzzle_answer2(uint32_t followers[]);
 
-uint32_t select_destination(uint32_t current_item, int datalen, uint32_t picked[PICKED_LEN]);
 
 int main(int argc, char **argv) {
     InputData input;
@@ -39,13 +42,10 @@ int main(int argc, char **argv) {
     if (followers == NULL) {
         return -1;
     }
-    printf("playing with followers:");
-    for (int i = 0; i < input.total_len + 1; i++) {
-        printf(" %d", followers[i]);
-    }
-    printf("\n");
-    play(followers, input.data[0], input.iterations);
-    printf("puzzle answer: %d\n", puzzle_answer2(followers));
+    //print_followers(followers, input.total_len);
+    play(followers, input.total_len, input.data[0], input.iterations);
+    print_all_elements(followers, input.total_len, 1, "");
+    //printf("puzzle answer 2: %d\n", puzzle_answer2(followers));
     free(followers);
     return 0;
 }
@@ -111,13 +111,13 @@ uint32_t* init_followers(InputData *in) {
     // followers[a] = b -> it means that b is after a in the list.
     // It is another way of storing the circle of numbers, which allows to lookup numbers in O(1).
     // followers[0] will be unused - for simplicity of implementation I want to use 1-based indexing here.
-    uint32_t *followers = (uint32_t*)malloc(sizeof(uint32_t) * (in->total_len + 1));
+    uint32_t *followers = malloc(sizeof(uint32_t) * (in->total_len + 1));
     if (followers == NULL) {
         return NULL;
     }
     memset(followers, 0, sizeof(uint32_t) * (in->total_len + 1));
-    // set links between given elements: 1, 2, ..., len(data)-1
     for (int i = 1; i < in->data_len; i++) {
+        // set links between given elements: 1, 2, ..., data_len
         int first = in->data[i - 1];
         int second = in->data[i];
         followers[first] = second;
@@ -126,21 +126,90 @@ uint32_t* init_followers(InputData *in) {
         // set link between last given element and first given element
         followers[in->data[in->data_len - 1]] = in->data[0];
     } else { // normal case for part2 - there are more elements than just given data
-        //TODO
+        followers[in->data[in->data_len - 1]] = in->data_len + 1; // link between last given element and first generated
+        for (int x = in->data_len + 1; x < in->total_len; x++) { // links between generated elements
+            followers[x] = x + 1;
+        }
+        followers[in->total_len] = in->data[0]; // link between last generated element and first given
     }
     return followers;
 }
 
-void play(uint32_t followers[], uint32_t current_item, int iterations) {
-    //TODO
+void play(uint32_t followers[], int total_len, uint32_t current_item, int iterations) {
+    int picked[PICKED_LEN];
+    for (int i = 0; i < iterations; i++) {
+        int item = current_item; // variable used to iterate over followers
+        // move 3 items after current_item to picked list:
+        for (int j = 0; j < PICKED_LEN; j++) {
+            item = followers[item];
+            picked[j] = item;
+        }
+        followers[current_item] = followers[picked[PICKED_LEN -1]]; // virtually remove picked items
+        // find destination for picked items:
+        item = select_destination(current_item, total_len, picked);
+        uint32_t after_destination = followers[item];
+        // add previously removed items after destination:
+        for (int j = 0; j < PICKED_LEN; j++) {
+            followers[item] = picked[j];
+            item = picked[j];
+        }
+        followers[picked[PICKED_LEN -1]] = after_destination;
+        // advance current_item:
+        current_item = followers[current_item];
+    }
+}
+
+void print_followers(uint32_t followers[], int total_len) {
+    printf("followers:");
+    for (int i = 0; i < total_len + 1; i++) {
+        printf(" %d", followers[i]);
+    }
+    printf("\n");
+}
+
+void print_all_elements(uint32_t followers[], int total_len, int start, char *sep) {
+    printf("all elements: ");
+    int i = 0;
+    int max_i = total_len - 1;
+    uint32_t curr = start;
+    for(;;) {
+        if (i >= max_i) {
+            printf("\nWARNING looping through followers stopped because reached max len (%d)", max_i);
+            return;
+        }
+        printf("%d%s", curr, sep);
+        curr = followers[curr];
+        if (curr == start) {
+            printf("\n");
+            return;
+        }
+    }
 }
 
 uint32_t puzzle_answer2(uint32_t followers[]) {
-    //TODO
-    return 0;
+    uint32_t after1 = followers[1];
+    uint32_t afterafter1 = followers[after1];
+    return after1 * afterafter1;
 }
 
-uint32_t select_destination(uint32_t current_item, int datalen, uint32_t picked[PICKED_LEN]) {
-    //TODO
-    return 0;
+uint32_t select_destination(uint32_t current_item, int total_len, uint32_t picked[PICKED_LEN]) {
+    int x = current_item - 1;
+    if (x == 0) {
+        x = total_len;
+    }
+    for (;;) {
+        int valid = 1;
+        for (int j = 0; j < PICKED_LEN; j++) {
+            if (x == picked[j]) {
+                valid = 0;
+                break;
+            }
+        }
+        if (valid) {
+            return x;
+        }
+        if (--x == 0) {
+            x = total_len;
+        }
+    }
 }
